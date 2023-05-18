738d6e5bc4b8a611ed4e2d085fa6c7c3815f4e9b
roman-supercode <roman.supercode@gmail.com>
2023-05-18 10:03:28 +0200

    css setup & navigation layout

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import App from "./App.tsx";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./pages/error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
